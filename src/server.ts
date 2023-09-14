97c16f47128fe31c892e94431ccf09a504324368
devjiwonchoi <devjiwonchoi@gmail.com>
2023-09-15 00:37:47 +0900

    chore: send html to client

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,7 @@ import cookieParser from 'cookie-parser'
 import axios from 'axios'
 import passport from 'passport'
 import { Strategy as GitHubStrategy, Profile } from 'passport-github2'
-import dotenv from 'dotenv'
-import { query } from './utils'
-
-dotenv.config()
+import { handleData, query, generateHTMLString } from './utils'
 
 interface CustomUser extends Profile {
   accessToken: string
@@ -24,11 +21,11 @@ passport.deserializeUser(function (obj: any, done) {
 passport.use(
   new GitHubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID ?? '',
-      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+      clientID: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
       callbackURL: `${process.env.BASE_URL}/auth/github/callback`,
     },
-    (accessToken: string, refreshToken: string, profile: any, done: any) => {
+    (accessToken: string, _refreshToken: string, profile: any, done: any) => {
       profile.accessToken = accessToken
       done(null, profile)
     }
@@ -36,7 +33,13 @@ passport.use(
 )
 
 const app = express()
-app.use(session({ secret: 'test', resave: false, saveUninitialized: false }))
+app.use(
+  session({
+    secret: process.env.EXPRESS_SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 app.use(cookieParser())
 app.use(passport.initialize())
 app.use(passport.session())
@@ -52,12 +55,16 @@ app.get(
   function (req, res) {
     const accessToken = (req.user as CustomUser).accessToken
     res.cookie('accessToken', accessToken)
-    res.redirect(`${process.env.BASE_URL}/fetch-discussions`)
+    res.redirect(`${process.env.BASE_URL}`)
   }
 )
 
-app.get('/fetch-discussions', async (req, res) => {
+app.get('/', async (req, res) => {
   const accessToken = req.cookies.accessToken
+  if (!accessToken) {
+    res.redirect('/auth/github')
+    return
+  }
   try {
     const response = await axios.post(
       'https://api.github.com/graphql',
@@ -68,8 +75,9 @@ app.get('/fetch-discussions', async (req, res) => {
         },
       }
     )
-
-    res.json({ data: response.data })
+    const data = handleData(response.data)
+    const htmlString = generateHTMLString(data)
+    res.send(htmlString)
   } catch (error) {
     res.json({ error })
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,98 @@
 export const query = `
 query {
-
   viewer {
-
     login
-
     repositoryDiscussionComments(onlyAnswers: true, first:100) {
-
       totalCount
-
       nodes {
-
         url
-
       }
-
     }
-
   }
+}
+`
 
-}`
+export function generateHTMLString({
+  username,
+  totalCount,
+  countPerRepo,
+}: {
+  username: string
+  totalCount: number
+  countPerRepo: { [key: string]: number }
+}) {
+  const repoCounts = Object.entries(countPerRepo).sort((a, b) => b[1] - a[1])
+
+  // Generate the HTML for the repo counts
+  const repoCountsHTML = repoCounts
+    .map(([repo, count]) => {
+      return `<p>${repo}: ${count}</p>`
+    })
+    .join('')
+
+  const svg = `<div class="github-card">
+  <h2>Answered GitHub Discussions</h2>
+  <p>Username: @${username}</p>
+  <p>Total Count: ${totalCount}</p>
+  <div class="repo-counts">
+      ${repoCountsHTML}
+    </div>
+</div>
+<style>
+.github-card {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  width: 300px;
+  margin: 20px;
+}
+
+.github-card h2 {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+.github-card p {
+  font-size: 14px;
+  margin: 8px 0;
+}
+.repo-counts {
+    margin-top: 10px;
+  }
+</style>`
+
+  return svg
+}
+
+export function handleData(data: any) {
+  const {
+    data: { viewer },
+  } = data
+  const {
+    login,
+    repositoryDiscussionComments: { totalCount, nodes },
+  } = viewer
+
+  let urls: string[] = []
+
+  const regexForRepo = /github\.com\/([^/]+)\/([^/]+)/
+  const countPerRepo = nodes.reduce((accumulator: any, node: any) => {
+    urls = [...urls, node.url]
+    const match = node.url.match(regexForRepo)
+    if (match) {
+      const [, owner, repo] = match
+      const fullRepo = `${owner}/${repo}`
+      accumulator[fullRepo] = (accumulator[fullRepo] || 0) + 1
+    }
+    return accumulator
+  }, {})
+
+  return {
+    username: login,
+    totalCount,
+    countPerRepo,
+    urls,
+  }
+}
